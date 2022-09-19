190ef3c6def754d711d008dc16d95d0f423126c5
Colby Prior <colby.prior@octopus.com>
2022-09-20 09:02:40 +1000

    change sqli style

--- RandomQuotes/Controllers/SQLIController.cs
+++ RandomQuotes/Controllers/SQLIController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using System.Data.SQLite;  
+using System.Data.SQLite;
+using System.Linq;
+using Microsoft.AspNetCore.Http.Extensions;
 
 
 namespace RandomQuotes.Controllers
@@ -11,16 +13,29 @@ namespace RandomQuotes.Controllers
     public class SQLIController : Controller
     {
         // testing normal: /sqli?name=Andrew
-        // testing exploit: /sqli?name=1%27%20or%20%271%27==%271
+        // testing exploit: /sqli?name=Octopus%27%20or%20%271%27==%271
         [HttpGet("sqli")]
         public IActionResult Get(string name)
         {
+            string clause = "";
+            List<string> list = new List<string>();
+            if (name.StartsWith("Octopus"))
+            {
+                list.Add("FirstName == '" + name + "';");
+            }
+            else
+            {
+                list.Add("FirstName == 'Andrew';");
+            }
+            
             SQLiteConnection conn2 = new SQLiteConnection("Data Source=Chinook_Sqlite.sqlite");  
             conn2.Open();  
   
             SQLiteCommand cmd2 = new SQLiteCommand(conn2);  
-            cmd2.CommandText = "select * from Employee where LastName == '" + name + "';";
-  
+            string whereClause = "where " + string.Join(" OR ", list);
+
+            cmd2.CommandText = "select * from Employee " + whereClause;
+            Console.WriteLine(cmd2.CommandText);
             SQLiteDataReader reader = cmd2.ExecuteReader();  
   
             
